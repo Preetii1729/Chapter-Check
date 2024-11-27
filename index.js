210701706294c21583555eed0fbc4e69b6257e1f
Preetii1729 <nirmadan.preeti2004@gmail.com>
2024-11-27 23:28:31 +0530

    Accesed all the input

--- index.js
+++ index.js
@@ -11,8 +11,18 @@ app.get("/", (req, res) => {
     res.render("index.ejs");
   });
 
-app.get("/new", (req, res)=> {
+app.post("/new", (req, res)=> {
   res.render("review.ejs");
+})
+
+app.post("/add", (req, res)=>{
+  let isbn = req.body.ISBN;
+  let title = req.body.title;
+  let author = req.body.user;
+  let rating = req.body.rating;
+  let review = req.body.review;
+  console.log("isbn: "+isbn+"\ntitle: "+title+"\nauthor: "+author+"\nrating: "+rating+"\nreview: "+ review+"\n");
+  res.redirect("/");
 })
 
 app.listen(port, () => {
