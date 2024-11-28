f33c3644b048d0c62eb3822ed97f7f67a2bfe78d
Preetii1729 <nirmadan.preeti2004@gmail.com>
2024-11-28 23:18:19 +0530

    Database created, connected and insert query is added

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 import express from "express";
 import bodyParser from "body-parser";
+import pg from "pg";
 
 const app = express();
 const port = 3000;
+
+const db = new pg.Client({
+  user: "postgres",
+  host: "localhost",
+  database: "ChapterCheck",
+  password: "preeti",
+  port: 5432,
+})
+db.connect();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
@@ -15,13 +25,18 @@ app.post("/new", (req, res)=> {
   res.render("review.ejs");
 })
 
-app.post("/add", (req, res)=>{
+app.post("/add", async(req, res)=>{
   let isbn = req.body.ISBN;
   let title = req.body.title;
   let author = req.body.user;
   let rating = req.body.rating;
   let review = req.body.review;
   console.log("isbn: "+isbn+"\ntitle: "+title+"\nauthor: "+author+"\nrating: "+rating+"\nreview: "+ review+"\n");
+
+  let new_entry = await db.query("Insert into reviews (isbn, title, author, rating, review) values ($1, $2, $3, $4, $5) returning *", 
+    [isbn, title, author, rating, review]
+  );
+  console.log("\n"+new_entry+"\n");
   res.redirect("/");
 })
 
@@ -29,3 +44,11 @@ app.listen(port, () => {
     console.log(`Server running on http://localhost:${port}`);
 });
   
+
+/*
+Make the database and start inserting.
+Sort by: title, rating
+Search author
+Search ISBN
+Delete review and edit option
+*/
